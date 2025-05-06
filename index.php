4e70e767b4d9334c54a4afe263ca031db0eb7d79
juwairenzhang <44838348+juwairenzhang@users.noreply.github.com>
2025-05-07 06:04:20 +0800

    Update index.php
    改为cookie登陆

--- index.php
+++ index.php
@@ -26,25 +26,35 @@ function brush()
             continue;
         }
         echo "----------------------------------------------------------\n";
-        $data = login($account['username'], $account['password']);
-        if ($data['username'] !== $account['username']) {
-            echo "登录失败（" . ($is_actions ? mb_substr($account['username'], 0, 1) . '***' : $account['username']) . "）\n";
+        if (empty($account['cookie'])) {
+            echo "Cookie 未配置，跳过账号（" . ($is_actions ? mb_substr($account['username'], 0, 1) . '***' : $account['username']) . "）\n";
             echo date("Y-m-d H:i:s\n");
             echo "----------------------------------------------------------\n";
             $err_accounts[] = $account;
             continue;
         }
+
+        echo "使用 Cookie 登录（" . ($is_actions ? mb_substr($account['username'], 0, 1) . '***' : $account['username']) . "）\n";
+        $data = get_info_with_cookie($account['cookie']);
+        if (empty($data['username'])) {
+            echo "Cookie 可能已失效，账号刷分失败（" . ($is_actions ? mb_substr($account['username'], 0, 1) . '***' : $account['username']) . "）\n";
+            echo date("Y-m-d H:i:s\n");
+            echo "----------------------------------------------------------\n";
+            $err_accounts[] = $account;
+            continue;
+        }
+
         echo "登录成功（" . ($is_actions ? mb_substr($account['username'], 0, 1) . '***' : $account['username']) . "）\n";
         if (!$is_actions) {
             echo "初始信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
         }
         echo "刷分中 ";
         for ($i = 31180; $i < 31210; $i++) {
-            http_get(str_replace('*', $i, 'https://ssdforum.org/space-uid-*.html'));
+            http_get(str_replace('*', $i, 'https://ssdforum.org/space-uid-*.html'), $account['cookie']);
             echo $i == 31209 ? "+ 完成\n" : "+";
             sleep(rand(5, 10));
         }
-        $data = get_info();
+        $data = get_info_with_cookie($account['cookie']);
         if (!$is_actions) {
             echo "结束信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
         }
@@ -63,96 +73,15 @@ function brush()
     notice($err_accounts);
 }
 
-
 /**
- * 登录
- * @param $username
- * @param $password
+ * 使用 Cookie 获取个人信息
+ * @param string $cookie_str
  * @return array
  */
-function login($username, $password)
-{
-    global $cookie;
-    $cookie = '';
-    $loginData = array(
-        'fastloginfield' => 'username',
-        'username' => $username,
-        'cookietime' => 2592000,
-        'password' => $password,
-        // 'formhash' => '751cf73e',
-        'quickforward' => 'yes',
-        'handlekey' => 'ls'
-    );
-    $cc_cookie = get_cc_cookie();
-    $response = http_post('https://ssdforum.org/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1', $loginData, $cc_cookie);
-    // 获取Cookie
-    preg_match_all('/set-cookie: (.*?);/i', $response, $matches);
-    if (count($cc_cookie) > 0) {
-        $cookie = http_build_query($cc_cookie, '', ';') . ';';
-    }
-    $cookie .= implode(';', $matches[1]);
-    return get_info();
-}
-
-/**
- * 获取CC防护的Cookie参数
- * @return array
- */
-function get_cc_cookie()
-{
-    $cc_cookie = [];
-    // 检测防CC机制的参数
-    $data = check_cc();
-    // 开启了防CC机制
-    if (count($data) === 4) {
-        // 密钥
-        $a = hex2bin($data['a']);
-        // 初始化向量 (IV)
-        $b = hex2bin($data['b']);
-        // 加密后的数据
-        $c = hex2bin($data['c']);
-        // AES CBC 解密
-        $value = openssl_decrypt($c, 'AES-128-CBC', $a, OPENSSL_NO_PADDING, $b);
-
-        $cc_cookie[$data['cookie']] = bin2hex($value);
-    }
-
-    return $cc_cookie;
-}
-
-
-/**
- * 检查是否开启CC防护
- * @return array
- */
-function check_cc()
+function get_info_with_cookie($cookie_str)
 {
     $data = [];
-    $html = file_get_contents('https://ssdforum.org/forum.php');
-
-    preg_match_all("/toNumbers\(\"(.*?)\"\)/", $html, $matches);
-    if (isset($matches[1]) && count($matches[1]) === 3) {
-        $data['a'] = $matches[1][0];
-        $data['b'] = $matches[1][1];
-        $data['c'] = $matches[1][2];
-    }
-
-    preg_match("/cookie=\"(.*?)=\"/", $html, $matches);
-    if (isset($matches[1])) {
-        $data['cookie'] = $matches[1];
-    }
-
-    return $data;
-}
-
-/**
- * 获取个人信息
- * @return array
- */
-function get_info()
-{
-    $data = [];
-    $html = http_get('https://ssdforum.org/home.php?mod=spacecp&ac=credit');
+    $html = http_get('https://ssdforum.org/home.php?mod=spacecp&ac=credit', $cookie_str);
     preg_match('/<a.*?title="访问我的空间">(.*)<\/a>/', $html, $matches);
     if (isset($matches[1])) {
         $data['username'] = $matches[1];
@@ -194,43 +123,17 @@ function get_info()
 /**
  * GET请求
  * @param $url
+ * @param string $custom_cookie
  * @return bool|string
  */
-function http_get($url)
+function http_get($url, $custom_cookie = '')
 {
-    global $cookie;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_URL, $url);
-    if (!empty($cookie)) {
-        curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+    if (!empty($custom_cookie)) {
+        curl_setopt($ch, CURLOPT_COOKIE, $custom_cookie);
     }
-    curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36');
-    curl_setopt($ch, CURLOPT_REFERER, 'https://ssdforum.org/');
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 600);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    return $response;
-}
-
-/**
- * POST请求
- * @param $url
- * @param $data
- * @return bool|string
- */
-function http_post($url, $data, $cookie = [])
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_COOKIE, http_build_query($cookie, '', '; '));
     curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36');
     curl_setopt($ch, CURLOPT_REFERER, 'https://ssdforum.org/');
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -251,20 +154,25 @@ function init_env()
 {
     global $config;
 
-    // 获取环境变量中的账号密码（格式user1@@@pass1---user2@@@pass2）
+    // 获取环境变量中的账号和 Cookie（格式user1@@@cookie1---user2@@@cookie2）
     $env_loc_accounts = getenv('LOC_ACCOUNTS');
     if ($env_loc_accounts) {
         $new_accounts = [];
         foreach (explode("---", $env_loc_accounts) as $env_account) {
             $account_parts = explode("@@@", $env_account);
-            if (count($account_parts) !== 2) {
-                continue;
+            if (count($account_parts) === 2) {
+                $username = $account_parts[0];
+                $cookie = trim($account_parts[1]);
+                $new_accounts[$username] = array(
+                    'username' => $username,
+                    'cookie' => $cookie,
+                    'last_brush' => '',
+                );
+            } elseif (count($account_parts) === 1) { // 仅有用户名，没有 Cookie，跳过
+                echo "警告：账号 '{$account_parts[0]}' 缺少 Cookie 配置，已跳过。\n";
+            } else {
+                echo "警告：账号配置 '{$env_account}' 格式不正确，已跳过。\n";
             }
-            $new_accounts[$account_parts[0]] = array(
-                'username' => $account_parts[0],
-                'password' => $account_parts[1],
-                'last_brush' => '',
-            );
         }
         // 更新和添加账户
         foreach ($config['accounts'] as $account) {
@@ -272,7 +180,7 @@ function init_env()
                 $new_accounts[$account['username']]['last_brush'] = $account['last_brush'];
             }
         }
-        // 最新的账号密码
+        // 最新的账号和 Cookie
         $config['accounts'] = array_values($new_accounts);
     }
 
@@ -299,7 +207,7 @@ function notice($err_accounts)
     }
     $username = array_column($err_accounts, 'username');
     $title = 'Hostloc 刷分失败';
-    $content = '账号（' . implode('，', $username) . '）刷分失败，请检查账号配置或程序';
+    $content = '账号（' . implode('，', $username) . '）刷分失败，请检查 Cookie 是否有效';
     $data = array(
         "key" => $tg_push_key,
         "text" => $title . "\n" . $content
